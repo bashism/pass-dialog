e5054da759089458486ab9b481b2e409d83ce748
Michael Ren <micron33@gmail.com>
2013-11-08 17:56:53 -0500

    implement support for password directories

--- pass-dialog.sh
+++ pass-dialog.sh
@@ -7,11 +7,12 @@ export PASSWORD_STORE_DIR
 pass_text="$(pass)"
 input_password_name="$(yad --entry --title="Passwords" --text="${pass_text}" )"
 
-num_matches=$(ls -l "${PASSWORD_STORE_DIR}"/"${input_password_name}"* | wc -l)
+matches=$(find "${PASSWORD_STORE_DIR}" -type f -name "${input_password_name}*")
 
-if [ ${num_matches} -eq 1 ]; then
-	pass_password="$(ls "${PASSWORD_STORE_DIR}"/"${input_password_name}"*)"
-	pass show "$(basename ${pass_password} .gpg)" -c
+if [ $(echo $matches | wc -w) -eq 1 ]; then
+	no_prefix="${matches#"${PASSWORD_STORE_DIR}/"}"
+	pass_password=${no_prefix%".gpg"}
+	pass show "${pass_password}" -c
 fi
 
-unset PASSWORD_STORE_DIR pass_text input_password_name num_matches pass_password
+unset PASSWORD_STORE_DIR pass_text input_password_name matches no_prefix pass_password
